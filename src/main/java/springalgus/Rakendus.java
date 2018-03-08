16718f065889a53ca7e5e5b2702e0839bcb94d22
Kaur K <kaurk@users.noreply.github.com>
2018-03-08 10:52:34 +0200

    Ülesande kirjeldus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Scanner;
 @RestController
 @SpringBootApplication
 public class Rakendus {
@@ -21,6 +22,14 @@ public class Rakendus {
     int korrutamine(int arv1, int arv2){
        return arv1*arv2;
     }
+    //harjutus1 lahendus
+
+      
+      
+    
+
+    
+    
     
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
